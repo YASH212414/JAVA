26b41f50ce98cc6c13ff3e3c2b531af267876725
mudim <mudim@LAPTOP-FV57J44O>
2023-06-13 22:11:57 +0530

    Commit

---
+++ CallingFunction/src/call_function/CallingFunction.java
@@ -0,0 +1,16 @@
+package call_function;
+
+public class CallingFunction {
+	
+	static void fullname() {
+        System.out.println("My full Name is 'mudimala yeshwanth goud'");
+    }
+	
+	public static void main(String[] args) {
+
+		        fullname();
+		     // NOTE : Here main method is fullname()
+
+	}
+
+}
