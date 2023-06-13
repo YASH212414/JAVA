7171e2d2e1ef620377e95d9938b4a6039d296ab9
mudim <mudim@LAPTOP-FV57J44O>
2023-06-13 22:08:35 +0530

    Commit

---
+++ print full name/src/print_full$name/Printfullname.java
@@ -0,0 +1,11 @@
+package print_full$name;
+
+public class Printfullname {
+
+	public static void main(String[] args) {
+		String name = "Mudhimala Yeshwanth Goud";
+		System.out.println("My name is " + name);
+
+	}
+
+}
