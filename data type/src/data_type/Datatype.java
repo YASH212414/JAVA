d015929c016cb62c7fedde1377da4eedfec6246f
mudim <mudim@LAPTOP-FV57J44O>
2023-06-13 22:10:13 +0530

    Commit

---
+++ data type/src/data_type/Datatype.java
@@ -0,0 +1,23 @@
+package data_type;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+	          
+	      double d = 333.1111;       
+	      char a  = 'A';            
+	      boolean b = true;  
+		  int i = 24;            
+	      float f = 33.1f; 
+	     
+	      System.out.println("This is a Double " + d);
+	        System.out.println("This is a Char " + a);
+	      System.out.println("This is a Boolean " + b);
+	      System.out.println("This is an Integer "+i);
+	        System.out.println("This is a Float " + f);
+	       
+	       
+	       
+	}
+
+}
