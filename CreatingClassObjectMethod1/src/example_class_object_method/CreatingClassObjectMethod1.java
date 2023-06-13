770b34ccc739fc094067d6d296488be36ee2cadf
mudim <mudim@LAPTOP-FV57J44O>
2023-06-13 21:31:12 +0530

    First Commit

---
+++ CreatingClassObjectMethod1/src/example_class_object_method/CreatingClassObjectMethod1.java
@@ -0,0 +1,15 @@
+package example_class_object_method;
+
+public class CreatingClassObjectMethod1 {
+	void MNIY(){
+		String a = "my name is yash ";
+        System.out.println(a);
+	}
+	
+	public static void main(String[] args) {
+		CreatingClassObjectMethod1 obj = new CreatingClassObjectMethod1();
+		 obj.MNIY();
+	
+	}
+
+}
