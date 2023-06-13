201b768707c5a99e6d03579474d178a75575be83
mudim <mudim@LAPTOP-FV57J44O>
2023-06-13 22:07:08 +0530

    Commit

---
+++ comments/src/comments/Comments.java
@@ -0,0 +1,18 @@
+package comments;
+
+public class Comments {
+	
+	 /* This is my first java comment  program.
+	    * This will print 'im looking handsome' as the output
+	    * This is an example of multi-line comments.
+	    */
+
+	public static void main(String[] args) {
+		
+		/** This is a Documentation comment */
+		// This is a Single line comment
+		 System.out.println("im looking handsome");
+		 
+	}
+
+}
